1850edc6c20a5f95e648c7660df72444dcc51449
Greg Burch <gb@gregburch.com>
2023-02-04 21:36:09 -0500

    Update miryoku_kludge_mousekeyspr.h

--- miryoku/miryoku_kludge_mousekeyspr.h
+++ miryoku/miryoku_kludge_mousekeyspr.h
@@ -5,7 +5,7 @@
 
 #include <dt-bindings/zmk/mouse.h>
 
-#define U_MOUSE_MOVE_MAX 1250
+#define U_MOUSE_MOVE_MAX 2450
 #define U_MOUSE_MOVE_EXPONENT 3
 #define U_MOUSE_MOVE_TIME 1500
 #define U_MOUSE_MOVE_DELAY 0
